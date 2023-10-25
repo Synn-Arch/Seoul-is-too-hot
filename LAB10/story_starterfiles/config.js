fbc00bdff1dfb88ed9e20f69baf2bdf505907a45
synnpower@gmail.com <synnpower@gmail.com>
2023-10-25 14:00:16 -0400

    change citation

--- LAB10/story_starterfiles/config.js
+++ LAB10/story_starterfiles/config.js
@@ -12,7 +12,7 @@ var config = {
     title: 'Public and Affordable Housing Flood Risk in NYC',
     subtitle: 'Rising Tides, Rising Challenges\nA Study of Flood Vulnerability in NYC Public and Affordable Housing',
     byline: 'By a SungHo Synn',
-    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    footer: 'Source: Risk Factor, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
             id: 'slug-style-id',
